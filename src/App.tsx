41f22b30af0ac7c6aab57d3a62e3fe6052fb8e13
topfragatlas <haydendodd.steam@gmail.com>
2025-03-01 01:32:44 -0600

    technologies component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Header from './assets/components/header'
 import Greeting from './assets/components/greeting'
+import Tech from './assets/components/tech'
 
 function App() {
 
   return (
     <>
       <div className='flex justify-self-center'>
-        <div className='flex flex-col w-dvw min-w-[40rem] max-w-[80rem]'>
+        <div className='flex flex-col w-dvw min-w-[30rem] max-w-[60rem] p-5'>
           <Header />
           <Greeting />
+          <Tech />
         </div>
       </div>
     </>
--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function header() {
   return (
-    <header className='flex justify-between sticky top-5 backdrop-blur-[5px] w-full mt-5 mb-5 text-2xl text-neutral-500'>
+    <header className='flex justify-between sticky top-5 backdrop-blur-[5px] w-full mt-5 mb-5 text-[1rem] text-neutral-500'>
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>home</a></li>
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>projects</a></li>
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>blog</a></li>

45e97cc050f7d06ce5293560fcfd2aa98d504fb3
topfragatlas <haydendodd.steam@gmail.com>
2025-03-02 21:46:31 -0600

    mobile optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import Header from './assets/components/header'
-import Greeting from './assets/components/greeting'
-import Tech from './assets/components/tech'
-import Footer from './assets/components/footer'
-import Featured from './assets/components/featured'
+import Home from "./pages/home"
 
 function App() {
 
   return (
     <>
-      <div className='flex justify-self-center'>
-        <div className='flex flex-col w-dvw min-w-[25rem] max-w-[60rem] pl-5 pr-5 pb-5'>
-          <Header />
-          <Greeting />
-          <Tech />
-          <Featured />
-          <Footer />
-        </div>
-      </div>
+      <Home />
     </>
   )
 }
--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,11 +1,12 @@
 
 export default function header() {
   return (
-    <div className='flex items-center sticky top-0 w-full h-15 backdrop-blur-[5px] bg-[#161616ab] z-99'>
-      <header className='flex justify-between w-full text-[1rem] text-neutral-400'>
+    <div className='mobile-resize-replaceblur flex items-center sticky top-0 w-full h-15 z-97'>
+      <div className='mobile-resize-hide absolute -right-1/2 w-[200dvw] h-15 backdrop-blur-[5px] bg-[#161616ab] z-98' />
+      <header className='flex justify-between w-full text-[1rem] text-neutral-400 z-99'>
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>home</a></li>
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>projects</a></li>
-        <li><a className='hover:text-neutral-100 transition-colors duration-150 line-through'>blog</a></li>
+        {/* <li><a className='hover:text-neutral-100 transition-colors duration-150 line-through'>blog</a></li> */}
         <li><a href='/' className='hover:text-neutral-100 transition-colors duration-150'>contact</a></li>
       </header>
     </div>

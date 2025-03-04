b5b5dc31ca5068bd720146ed8b0e02f0e4f74d67
topfragatlas <haydendodd.steam@gmail.com>
2025-03-04 02:26:42 -0600

    Contact form | [Fix-2] | Testing

--- src/assets/components/contactlinks.jsx
+++ src/assets/components/contactlinks.jsx
@@ -1,29 +1,28 @@
-import React from "react"
 
 export default function ContactLinks() {
-  const [ result, setResult ] = React.useState('');
-
   const onSubmit = async (event) => {
-    // event.preventDefault();
-    setResult('Sending...');
+    event.preventDefault();
     const formData = new FormData(event.target);
 
     formData.append('access_key', '732a81d9-cb4f-49d5-8d71-38e3e8d1545a');
 
-    const response = await fetch('https://api.web3forms.com/submit', {
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch('https://api.web3forms.com/submit', {
       method: 'POST',
-      body: formData
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: json,
     });
 
-    const data = await response.json();
-
-    if (data.sucess) {
-      setResult('Form submitted successfully!');
-      console.log('Message sent.')
+    if (res.sucess) {
+      console.log('Message sent.', res);
       event.target.reset();
     } else {
-      console.log('Error', data);
-      setResult(data.message);
+      console.error('Error', res);
     }
   };
 
@@ -41,7 +40,6 @@ export default function ContactLinks() {
           <textarea name='message' className='w-full h-50 p-3 mt-3 border border-neutral-500 rounded-md' placeholder='Leave some feedback, career opportunities or just to say hi etc.' required />
           <button type='submit' className='cursor-pointer flex items-center justify-center w-full p-3 bg-neutral-100 hover:bg-neutral-300 text-neutral-900 rounded-md font-bold mt-5'>Send message <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg></button>
         </form>
-        <span>{result}</span>
       </div>
 
       <div className='flex flex-col gap-3'>
